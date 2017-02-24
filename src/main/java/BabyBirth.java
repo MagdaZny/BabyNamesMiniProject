4b54545e0dfa5cc9c75126170f7221e2fa14f352
MagdaZny <zieleznym6@gmail.com>
2017-02-24 21:22:09 +0100

    Add highestRanking method

--- src/main/java/BabyBirth.java
+++ src/main/java/BabyBirth.java
@@ -49,13 +49,57 @@ public class BabyBirth {
         return count+1;
     }
 
+public static String getName(int year, long rank, String gender) {
+    FileResource fr = new FileResource("us_babynames/us_babynames_by_year/yob"+year+".csv");
+
+    long count = 0;
+    for (CSVRecord record : fr.getCSVParser(false)) {
+        if (record.get(1).equals(gender)) {
+            int currentRecordNum = Integer.parseInt(record.get(2));
+            for (CSVRecord recordd : fr.getCSVParser(false)) {
+                if (recordd.get(1).equals(gender)) {
+                    int currentRecorddNum = Integer.parseInt(recordd.get(2));
+                    if (currentRecorddNum > currentRecordNum) count++;
+                }
+            }
+        }
+        if ((count+1)==rank) return record.get(0);
+
+    }
+    return "NO NAME";
+}
+ public static String whatIsNameInYear(String name, int year, int newYear, String gender){
+     long rank = getRank(year, name, gender);
+     String newName = getName(newYear, rank, gender);
+     return  newName;
+ }
+
+    public static int yearOfHighestRank(String name, String gender){
+     long highestSoFar=0;
+     int year=0;
+     for (int i=1880; i<=2014; i++){
+         long currentRank=getRank(i, name, gender);
+         if(currentRank>highestSoFar) {
+             highestSoFar=currentRank;
+             year=i;
+         }
+     }
+     return year;
+    }
+
+    public static double getAverageRank(String name, String gender){
+
+    }
 
 
     public static void main(String[] arg){
-        FileResource file = new FileResource("yob2012short.csv");
-        printNames(file);
-        List<Integer> born = totalBirth(file);
-        System.out.println("Number born: "+born.get(0) +" number of boys born: "+born.get(1));
-        System.out.println("Olivia in 2012 was the " + getRank(2012, "Olivia", "F")+ " most popular name among girls born.");
+//        FileResource file = new FileResource("yob2012short.csv");
+//        printNames(file);
+//        List<Integer> born = totalBirth(file);
+//        System.out.println("Number born: "+born.get(0) +" number of boys born: "+born.get(1));
+//        System.out.println("Olivia in 2012 was the " + getRank(2012, "Olivia", "F")+ " most popular name among girls born.");
+         // System.out.println("2nd girl in 2012 was " + getName(2012, 2, "F") );
+         // System.out.println("Emma in 2014 was "+whatIsNameInYear("Emma", 1991, 2000, "F"));
+     System.out.println("Emma was the most popular nam in "+yearOfHighestRank("Emma", "F"));
     }
 }
